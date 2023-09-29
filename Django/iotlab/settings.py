ccde142069f43d31ee4d1610c4985e1511afa827
Jeronimo <mestrej88@gmail.com>
2023-09-29 13:49:06 -0300

    retirando DIR templates/static

--- Django/iotlab/settings.py
+++ Django/iotlab/settings.py
@@ -56,7 +56,6 @@ ROOT_URLCONF = 'iotlab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
